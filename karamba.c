298e423f8ff5dcb2f28a76ab445334dcd79a7ea6
Ruslan <pupsor@gmail.com>
2012-09-09 14:09:17 -0700

    fix leaks, got segfault

--- karamba.c
+++ karamba.c
@@ -20,6 +20,7 @@ static int handle_options(const char ***argv, int *argc, struct Config *k)
   while (*argc > 0) {
     const char *cmd = (*argv)[0];
 
+
     if(!strcmp(cmd, "--help") || !strcmp(cmd, "-h") || !strcmp(cmd, "-?"))
     {
       printf("Karamba version 0.0\n");
@@ -87,7 +88,6 @@ Queue queue_add(struct Config *k)
       printf("%s", buffer);
       dir_ptr = malloc((int) strlen(buffer) + 1);
       strncpy(dir_ptr, buffer, strlen(buffer) - 1);
-      // strcpy(dir_ptr, buffer);
       Enqueue(dir_ptr, Q);
     }
     printf("Current position in the file : %ld\n", ftell(fp));
@@ -104,65 +104,79 @@ void scan(Queue Q, struct Config *k)
 {
   char *dir_ptr;
   char *url;
-  // long http_code;
+  long http_code;
 
-  // CURL *curl;
-  // CURLcode response;
+  CURL *curl;
+  CURLcode response;
 
   // init curl and set 301 follow
-  // curl = curl_easy_init();
-  // curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION,1);
+  curl = curl_easy_init();
+  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION,1);
 
 
-  printf("Print all 10 elements...\n");
   while (!IsEmpty(Q)) {
     dir_ptr = Front(Q);
     if(dir_ptr != NULL) {
-      // url = malloc(strlen(k->target_url) + strlen(dir_ptr) + sizeof(char) * 2);
-      // strcat(url, k->target_url);
-      // strcat(url, "/");
-      // strcat(url, dir_ptr);
-      // strcat(url, "/");
+      // BUG: 10 why is 2 reason of segment fault. 
+      // FIX: malloc
+      // ADD: Threads
+      url = malloc(strlen(k->target_url) + strlen(dir_ptr) + 10); 
+      strcat(url, k->target_url);
+      strcat(url, "/");
+      strcat(url, dir_ptr);
+      strcat(url, "/");
 
-      // printf("%s\n", url);
-      // free(url);
-      // url = "http://google.com/";
-      // curl_easy_setopt(curl, CURLOPT_URL,url);
-      // response = curl_easy_perform(curl);
-      // curl_easy_getinfo (curl, CURLINFO_RESPONSE_CODE, &http_code);
+      printf("%s\n", url);
+      curl_easy_setopt(curl, CURLOPT_URL,url);
+      response = curl_easy_perform(curl);
+      curl_easy_getinfo (curl, CURLINFO_RESPONSE_CODE, &http_code);
+      
+      if (http_code == 200 || http_code == 403) {
+        printf("FOUND %s (response code %ld)\n", url, http_code);
+        printf("%s (HTTP code %ld)\n", url,http_code);
+      }  
+
+      free(url);
     }
     Dequeue(Q);
     free(dir_ptr);
   }
+  curl_easy_cleanup(curl);
 }
 
 int main(int argc, char const *argv[])
 {
-  struct Config *karamba_conf = malloc(sizeof(struct Config));
-  assert(karamba_conf != NULL);
-  handle_options(&argv, &argc, karamba_conf);
+  struct Config karamba_conf;
+
+  // if less 2 arg then exit
+  if (argc < 2){
+    printf("usage: %s\n", karamba_usage_string);
+    exit(1);
+  }
+
+  handle_options(&argv, &argc, &karamba_conf);
 
   // set fseek at the begining of the world list
-  karamba_conf->position = 0;
+  karamba_conf.position = 0;
 
-  if(karamba_conf->target_url == NULL || karamba_conf->dictonary == NULL) {
+  if(karamba_conf.target_url == NULL || karamba_conf.dictonary == NULL) {
     printf("usage: %s\n", karamba_usage_string);
     return 1;
   }
-  
+
   Queue Q;
 
-  while(karamba_conf->position >= 0)
+  while(karamba_conf.position >= 0)
   {
-    Q = queue_add(karamba_conf);
-    scan(Q, karamba_conf);
+    Q = queue_add(&karamba_conf);
+    scan(Q, &karamba_conf);
   }
 
-  printf("Position is : %ld\n", karamba_conf->position);
+  printf("Position is : %ld\n", karamba_conf.position);
   printf("\n\n");
   printf("Dispose of the queue...\n");
+  
   DisposeQueue(Q);
 
-  free(karamba_conf);
   return 0;
 }
